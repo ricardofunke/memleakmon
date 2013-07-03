5a697dd1a32e2271cc79d53d7cc685253240e470
ricardofunke <ricardo.funke@liferay.com>
2013-07-03 16:29:05 -0300

    change gcold to gcutil, there's no need to calculate percentage

--- memleakmon.sh
+++ memleakmon.sh
@@ -6,27 +6,19 @@ TDUMP_FILE_DIR="/app/dumps/threadDumps"
 
 JAVA_PID=$(pgrep java); if [[ $? -ne 0 ]]; then echo "No java pid running! Exiting..."; exit 1; fi
 
-percent_usage() {
-  OC=${1%.*}
-  OU=${2%.*}
-  percent=$(( OU * 100 / OC ))
-  echo ${percent}
+watch_mem() {
+  jstat -gcutil $JAVA_PID 1 1 | sed '1d' | awk '{print $4}'
 }
 
-
-do_monitor() {
-  jstat -gcold $JAVA_PID 3000 1 | sed '1d' | awk '{print $3,$4}'
-}
-
-run_dumps() {
+dump() {
     su $APP_USR -c "jmap -F -dump:format=b,file=\"${MDUMP_FILE_DIR}/jvm-$(hostname)-$(date +%Y%m%d%H).mdump\" $JAVA_PID"
     for n in {1..5}; do su $APP_USR -c "jstack -F $JAVA_PID > \"${TDUMP_FILE_DIR}/jvm-$(hostname)-$n-$(date +%Y%m%d%H).tdump\""; sleep 5; done
     exit 0
 }
 
-daemonize() {
+run() {
   while true; do
-    mem_usage=$(percent_usage $(do_monitor))
+    mem_usage=$(watch_mem)
 
     if [[ $mem_usage -ge 95 ]]; then
       (( verify++ ))
@@ -35,10 +27,10 @@ daemonize() {
     fi
 
     if [[ $verify -ge 4 ]]; then
-      run_dumps
+      dump
     fi
     sleep 15
   done
 }
 
-daemonize
+run
