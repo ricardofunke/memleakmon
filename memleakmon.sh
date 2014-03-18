aac21a5781cf908b199cb696bb4854b379f57216
ricardofunke <ricardo.funke@liferay.com>
2014-03-18 11:13:18 -0300

    adding more triggers

--- memleakmon.sh
+++ memleakmon.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-APP_USR=tomcat
-MDUMP_FILE_DIR="dumps/memoryDumps"
-TDUMP_FILE_DIR="dumps/threadDumps"
-ERROR_FILE=memleakmon.err
-MEM_LIMIT=95
+APP_USR=liferay
+MDUMP_FILE_DIR="/opt/dumps/memoryDumps"
+MHISTO_FILE_DIR="/opt/dumps/memoryHisto"
+TDUMP_FILE_DIR="/opt/dumps/threadDumps"
+ERROR_FILE_DIR="/opt/dumps/error"
+MELIMIT=90
+DBLIMIT=220
+LDLIMIT=7
+
+sleep 20
 
 JAVA_PID=$(pgrep java); if [[ $? -ne 0 ]]; then echo "No java pid running! Exiting..."; exit 1; fi
 
@@ -12,24 +17,42 @@ watch_mem() {
   jstat -gcutil $JAVA_PID 1 1 | sed '1d' | awk '{print $4}' | tr , .
 }
 
+watch_db() {
+  netstat -an | grep ':5432' | grep ESTAB | wc -l
+}
+
+watch_load() {
+  cat /proc/loadavg | awk '{print $2}' | tr , .
+}
+
 dump() {
-    su $APP_USR -c "jmap -dump:format=b,file=\"${MDUMP_FILE_DIR}/jvm-$(hostname)-$(date +%Y%m%d%H).mdump\" $JAVA_PID 2>> $ERROR_FILE" 
-    for n in {1..5}; do su $APP_USR -c "jstack $JAVA_PID > \"${TDUMP_FILE_DIR}/jvm-$(hostname)-$n-$(date +%Y%m%d%H).tdump\" 2>> $ERROR_FILE"; sleep 5; done
-    exit 0
+
+    local dir=$(date +%Y%m%d%H%M)
+    mkdir -p ${MDUMP_FILE_DIR}/${dir} ${MHISTO_FILE_DIR}/${dir} ${TDUMP_FILE_DIR}/${dir} ${ERROR_FILE_DIR}/${dir}
+    chown ${APP_USR}: ${MDUMP_FILE_DIR}/${dir} ${MHISTO_FILE_DIR}/${dir} ${TDUMP_FILE_DIR}/${dir} ${ERROR_FILE_DIR}/${dir}
+
+#    su $APP_USR -c "jmap -dump:format=b,file=\"${MDUMP_FILE_DIR}/${dir}/jvm-$(hostname).mdump\" $JAVA_PID 2>> ${ERROR_FILE_DIR}/${dir}/error.log" 
+    su -s /bin/bash $APP_USR -c "jmap -histo:live $JAVA_PID > \"${MHISTO_FILE_DIR}/${dir}/jvm-$(hostname).mhisto\" 2>> ${ERROR_FILE_DIR}/${dir}/error.log"
+    for n in {1..15}; do su -s /bin/bash $APP_USR -c "jstack $JAVA_PID > \"${TDUMP_FILE_DIR}/${dir}/$n-jvm-$(hostname).thdump\" 2>> ${ERROR_FILE_DIR}/${dir}/error.log"; sleep 10; done
+
+#    exit 0
 }
 
 run() {
   while true; do
-    mem_usage=$(watch_mem)
+    #meusage=$(watch_mem)
+    dbusage=$(watch_db)
+    ldusage=$(watch_load)
 
-    if [[ $(echo $mem_usage '>=' $MEM_LIMIT | bc -l) -eq 1 ]]; then
+    if [[ ( $(echo $dbusage '>=' $DBLIMIT | bc -l) -eq 1 ) || ( $(echo $ldusage '>=' $LDLIMIT | bc -l) -eq 1  ) ]]; then
       (( verify++ ))
     else
       (( verify=0 ))
     fi
 
-    if [[ $verify -ge 4 ]]; then
+    if [[ $verify -gt 1 ]]; then
       dump
+      sleep 15
     fi
     sleep 15
   done
