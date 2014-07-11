c2a0764772a75322cae0d67d3a155fb937d72e54
ricardofunke <ricardo.funke@liferay.com>
2014-07-11 18:30:07 -0300

    fixing heap memory calculation

--- memleakmon.sh
+++ memleakmon.sh
@@ -14,7 +14,10 @@ sleep 20
 JAVA_PID=$(pgrep java); if [[ $? -ne 0 ]]; then echo "No java pid running! Exiting..."; exit 1; fi
 
 watch_mem() {
-  jstat -gcutil $JAVA_PID 1 1 | sed '1d' | awk '{print $4}' | tr , .
+  local heap_size=$(jstat -gccapacity $JAVA_PID 1 1 | sed '1d' | awk '{print $2, "+", $8}' | tr , . | bc)
+  local heap_usage=$(jstat -gc $JAVA_PID 1 1 | sed '1d' | awk '{print $3, "+", $4, "+", $6, "+", $8}' | tr , . | bc)
+  local percent_usage=$(echo "($heap_usage * 100) / $heap_size)" | bc)
+  echo $percent_usage
 }
 
 watch_db() {
@@ -31,7 +34,7 @@ dump() {
     mkdir -p ${MDUMP_FILE_DIR}/${dir} ${MHISTO_FILE_DIR}/${dir} ${TDUMP_FILE_DIR}/${dir} ${ERROR_FILE_DIR}/${dir}
     chown ${APP_USR}: ${MDUMP_FILE_DIR}/${dir} ${MHISTO_FILE_DIR}/${dir} ${TDUMP_FILE_DIR}/${dir} ${ERROR_FILE_DIR}/${dir}
 
-#    su $APP_USR -c "jmap -dump:format=b,file=\"${MDUMP_FILE_DIR}/${dir}/jvm-$(hostname).mdump\" $JAVA_PID 2>> ${ERROR_FILE_DIR}/${dir}/error.log" 
+#    su $APP_USR -c "jmap -dump:format=b,file=\"${MDUMP_FILE_DIR}/${dir}/jvm-$(hostname).hprof\" $JAVA_PID 2>> ${ERROR_FILE_DIR}/${dir}/error.log" 
     su -s /bin/bash $APP_USR -c "jmap -histo:live $JAVA_PID > \"${MHISTO_FILE_DIR}/${dir}/jvm-$(hostname).mhisto\" 2>> ${ERROR_FILE_DIR}/${dir}/error.log"
     for n in {1..15}; do su -s /bin/bash $APP_USR -c "jstack $JAVA_PID > \"${TDUMP_FILE_DIR}/${dir}/$n-jvm-$(hostname).thdump\" 2>> ${ERROR_FILE_DIR}/${dir}/error.log"; sleep 10; done
 
